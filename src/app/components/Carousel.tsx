8c763adc38d0f1c03344840fab61312c3cd2b73d
iugorsette <iugorsette@gmail.com>
2023-05-30 22:07:16 -0300

    fix: fix images and Location

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import image1 from "../../../public/picture/image-1.png";
-import image2 from "../../../public/picture/image-2.png";
-import image3 from "../../../public/picture/image-3.png";
-import image4 from "../../../public/picture/image-4.png";
-import image5 from "../../../public/picture/image-5.png";
+import image1 from "../../../public/picture/image-1.jpeg";
+import image2 from "../../../public/picture/image-2.jpeg";
+import image3 from "../../../public/picture/image-3.jpeg";
+import image4 from "../../../public/picture/image-4.jpeg";
+import image5 from "../../../public/picture/image-5.jpeg";
+import image6 from "../../../public/picture/image-6.jpeg";
 
 export const CarouselComponent = () => {
   return (
@@ -20,22 +21,25 @@ export const CarouselComponent = () => {
       transitionTime={500}
       showArrows={true}
       showIndicators={true}
-      centerMode={true}
+      className="w-full h-full"
     >
       <div>
-        <Image src={image1} alt="Renata Nails Design" />
+        <Image src={image1} alt="Renata Nails Design"  />
       </div>
       <div>
-        <Image src={image2} alt="Renata Nails Design" />
+        <Image src={image2} alt="Renata Nails Design"  />
       </div>
       <div>
-        <Image src={image3} alt="Renata Nails Design" />
+        <Image src={image3} alt="Renata Nails Design"  />
       </div>
       <div>
-        <Image src={image4} alt="Renata Nails Design" />
+        <Image src={image4} alt="Renata Nails Design"  />
       </div>
       <div>
-        <Image src={image5} alt="Renata Nails Design" />
+        <Image src={image5} alt="Renata Nails Design"  />
+      </div>
+      <div>
+        <Image src={image6} alt="Renata Nails Design"  />
       </div>
     </Carousel>
   );
--- src/app/components/Location.tsx
+++ src/app/components/Location.tsx
@@ -5,7 +5,7 @@ export function Location() {
   <h2 className="text-center text-2xl text-zinc-800">Onde nos encontrar</h2>
 </div>
 <div className="rounded-3xl overflow-hidden flex flex-col items-center justify-center md:flex-row md:items-end md:justify-end">
-  <p className="text-center text-xl text-zinc-800 p-5 md:p-0">📍 Av. Dr. José Mariano, 389 - Palmeiras, Pte. Nova - MG, 35430-228</p>
+  <p className="text-center text-xl text-zinc-800 p-5 md:p-0">📍 Av. Dr. José Mariano, 385 - Palmeiras, Pte. Nova - MG, 35430-228</p>
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d934.8277488323952!2d-42.8936220748588!3d-20.4112794244504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa496503deb884b%3A0x3a17c421bec1f24b!2sEmbella%20Produtos%20de%20Beleza!5e0!3m2!1spt-BR!2sbr!4v1685404817456!5m2!1spt-BR!2sbr"
     width="600"
